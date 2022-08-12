461decfdab8e8908b238f9b9f12c0448a1df2cef
ChyunKim <co1995@naver.com>
2022-08-12 23:34:46 +0900

    feat : 수정 기능 추가

--- todo/src/component/TodoInput.js
+++ todo/src/component/TodoInput.js
@@ -1,23 +1,46 @@
 import React from "react";
 
-const TodoInput = ({listAdd,todo}) => {
-    const [input, setInput] = React.useState('')
+const TodoInput = ({ todo, setTodo }) => {
+  const [input, setInput] = React.useState("");
 
-    const eventClick = (e) => {
-        if( input.length === 0 ) {
-          alert('할일을 입력하세요!')
-        }else {
-          listAdd(input)
-          setInput('')
-        }  
+  const listAdd = (value) => {
+    // 기존 list에 배열을 concat을 사용하여 list 추가
+    // if (!todo.indexOf(value)) {
+    //   alert("이미 추가된 리스트입니다!");
+    // } else {
+    //   const li = todo.concat(value);
+    //   setTodo(li);
+    // }
+    const isDulicate = todo.some((item) => item === value);
+    if (isDulicate) {
+      alert("이미 추가된 리스트입니다!");
+      return;
     }
-  
-    return (
-        <>
-          <input value = {input} type = "text" onChange={(e)=>setInput(e.target.value)} />
-          <button className="plusbtn" onClick={eventClick}>+</button>
-        </>
-    );
-  }
-  
-  export default TodoInput;
+    setTodo((todo) => [...todo, value]);
+  };
+
+  const eventClick = (e) => {
+    if (input.length === 0) {
+      alert("할일을 입력하세요!");
+    } else {
+      listAdd(input);
+      setInput("");
+    }
+  };
+
+  return (
+    <>
+      <input
+        value={input}
+        type="text"
+        onChange={(e) => setInput(e.target.value)}
+      />
+
+      <button className="plusbtn" onClick={eventClick}>
+        +
+      </button>
+    </>
+  );
+};
+
+export default TodoInput;
